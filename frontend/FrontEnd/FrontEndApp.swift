e5fbc4a42bedb91289feb820cd6f956784b7c26c
Sibbons Shrestha <SibbonsSS@gmail.com>
2022-01-23 13:41:25 -0800

    frontend hello world done

---
+++ frontend/FrontEnd/FrontEndApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontEndApp.swift
+//  FrontEnd
+//
+//  Created by Sibbons Shrestha on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FrontEndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
