665d60e7d40b44100b749c398a7f9c041caebbed
unknown <brookeqingzhang@gmail.com>
2022-01-25 17:38:13 -0800

    added get_items endpoint

--- backend/flaskapi.py
+++ backend/flaskapi.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
     return "Hello World!"
+
+@app.route('/get_items')
+def get_users():
+    return {}
 
 app.run(host='0.0.0.0', port=5000)
