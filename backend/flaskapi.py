1577f6fdd56adcfcde80206023363d9ceee22d01
brookezhang <brookeqingzhang@gmail.com>
2022-02-02 17:15:47 -0800

    Adding possible endpoints

--- backend/flaskapi.py
+++ backend/flaskapi.py
@@ -7,7 +7,16 @@ def hello():
     return "Hello World!"
 
 @app.route('/get_items')
-def get_users():
-    return {}
+def get_items():
+    pass
+
+@app.route('/post_picture')
+def post_picture():
+    pass
+
+# @app.route('/get_items', methods=["POST"])
+# def quiz(): 
+#     subject= request.form.get('sub')
+#     return render_template("quiz.html",subject=subject) 
 
 app.run(host='0.0.0.0', port=5000)
