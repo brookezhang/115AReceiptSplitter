ed5638c5df186db12681b50ef0a7acef515aa3f3
Sibbons Shrestha <SibbonsSS@gmail.com>
2022-02-07 17:03:38 -0800

    added global functionality to naming

--- frontend/FrontEnd/ContentView.swift
+++ frontend/FrontEnd/ContentView.swift
@@ -1,13 +1,35 @@
-//
-//  ContentView.swift
-//  FrontEnd
-//
-//  Created by Sibbons Shrestha on 1/23/22.
-//
-
 import SwiftUI
 
+// Our observable object class
+class Person: ObservableObject {
+    @Published var nameList = [String]()
+}
+
+// A view that expects to find a GameSettings object
+// in the environment, and shows its score.
+struct ScoreView: View {
+    @EnvironmentObject var persons: Person
+    var body: some View {
+        
+            List{
+                Section{
+                    ForEach(persons.nameList, id:\.self) {item in
+                        VStack(alignment: .leading){
+                            Text(item).font(.headline)
+                        }
+                    }.onDelete(perform:{
+                        indexSet in persons.nameList.remove(atOffsets:indexSet)
+                    })
+                }
+            }
+        }
+    }
+
+
+
 struct ContentView: View {
+    @StateObject var persons = Person()
+    //persons.nameList
     @State var text: String = ""
     @State var namesList = [String]()
     var body: some View {
@@ -18,7 +40,7 @@ struct ContentView: View {
                         TextField("Peter Parker", text: $text)
                         Button(action: {
                             if !text.isEmpty{
-                                namesList.insert(text, at:0)
+                                persons.nameList.insert(text, at:0)
                                 text = ""
                             }
                         }, label:{
@@ -27,24 +49,59 @@ struct ContentView: View {
                     }
                 }
                 Section{
-                    ForEach(namesList, id:\.self) {item in
+                    ForEach(persons.nameList, id:\.self) {item in
                         VStack(alignment: .leading){
                             Text(item).font(.headline)
                         }
                     }.onDelete(perform:{
-                        indexSet in namesList.remove(atOffsets:indexSet)
+                        indexSet in persons.nameList.remove(atOffsets:indexSet)
                     })
                 }
+                NavigationLink(destination: ScoreView()) {
+                    Text("See Names in different view")
+                }
             }.navigationTitle("Add Names")
-        }
+        }.environmentObject(persons)
     }
 
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+//struct ContentView: View {
+//    @State var text: String = ""
+//    @State var namesList = [String]()
+//    var body: some View {
+//        NavigationView{
+//            List{
+//                Section(header: Text("Enter New Name Below")){
+//                    HStack{
+//                        TextField("Peter Parker", text: $text)
+//                        Button(action: {
+//                            if !text.isEmpty{
+//                                namesList.insert(text, at:0)
+//                                text = ""
+//                            }
+//                        }, label:{
+//                                Text("Add")
+//                        })
+//                    }
+//                }
+//                Section{
+//                    ForEach(namesList, id:\.self) {item in
+//                        VStack(alignment: .leading){
+//                            Text(item).font(.headline)
+//                        }
+//                    }.onDelete(perform:{
+//                        indexSet in namesList.remove(atOffsets:indexSet)
+//                    })
+//                }
+//            }.navigationTitle("Add Names")
+//        }
+//    }
+//
+//}
---
+++ frontend/FrontEnd/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  FrontEnd
+//
+//  Created by Sibbons Shrestha on 2/5/22.
+//
+
+import SwiftUI
+
+//struct Person {
+//    let name: String
+//    let totalOwed: Int
+//    
+//    init(name: String){
+//        self.name = name
+//    }
+//}
+//
+//struct PersonsList{
+//    static let names = [Person]()
+//}
