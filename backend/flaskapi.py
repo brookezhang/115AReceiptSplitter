2d5e3558fba40beb3367fc66b9de64b56ce94cd0
JohnHLe <le.john2029@gmail.com>
2022-02-28 18:51:58 -0800

    Added Error Codes
    Added Error Codes to Flask file

--- backend/flaskapi.py
+++ backend/flaskapi.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from AzureReceiptRec import AzureReceipt
+import json
 app = Flask(__name__)
 
 @app.route('/get_items', methods=['POST'])
@@ -7,6 +8,10 @@ def get_items():
     reciept = request.json
     imgStr = reciept["base64"]
     r = AzureReceipt()
-    return r.get(imgStr) 
+    if r == 'Error':
+        return json.dumps("Error"), 400
+    else:
+        return r.get(imgStr), 200
+
 
 app.run(host='127.0.0.1', port=5000) # if on PC, use '0.0.0.0' instead
